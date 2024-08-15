aacb832d626149adc2b88dd6a57be148dba8b0b9
Oleh <olehkavetskyi@gmail.com>
2024-08-15 17:08:33 +0300

    added Announcement entity

---
+++ src/Domain/Entities/Announcement.cs
@@ -0,0 +1,9 @@
+﻿namespace Domain.Entities;
+
+public class Announcement
+{
+    public Guid Id { get; set; }
+    public string Title { get; set; } = null!;
+    public string Description { get; set; } = null!;
+    public DateTime DateAdded { get; set; }
+}
